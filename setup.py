3f883151364eecd737ce6190b5a98bb765d1f6f6
WaterKnight1998 <dvdlacallecastillo@gmail.com>
2020-09-08 23:47:51 +0200

    Windows installation works correctly now.

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ from codecs import open
 
 # For installing PyTorch and Torchvision in Windows
 import sys
-import os
 import subprocess
-from setuptools.command.install import install
 
 # Get the long description from the README file
 with open('README.md', 'r', encoding='utf-8') as f:
@@ -34,43 +32,37 @@ with open('semtorch/__init__.py', 'r', encoding='utf-8') as f:
     version = f.readline().split('=')[-1].strip().replace('"','')
 
 
-class PreInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        global install_reqs
 
-        print(f"Platform: {sys.platform}")
-        if sys.platform in ['win32','cygwin','windows']:
-            torch_version = "torch>=1.6.0,<2.0.0"
-            torchvision_version = "torchvision>=0.7.0,<1.0.0"
-
-            for requirement in install_reqs:
-                if "torch" in requirement:
-                    torch_version = requirement
-                if "torchvision" in requirement:
-                    torchvision_version = requirement
-            
-            install_reqs = remove_requirements(install_reqs,'torch')
-            install_reqs = remove_requirements(install_reqs,'torchvision')
-
-            print('Trying to install PyTorch and Torchvision!')
-            code = 1
-            try:
-                code = subprocess.call(['pip', 'install', torch_version, torchvision_version, '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
-                if code != 0:
-                    raise Exception('PyTorch and Torchvision installation failed !')
-            except:
-                try:
-                    code = subprocess.call(['pip3', 'install', torch_version, torchvision_version, '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
-                    if code != 0:
-                        raise Exception('PyTorch and Torchvision installation failed !')
-                except:
-                    print('Failed to install PyTorch, please install PyTorch and Torchvision manually following the simple instructions at: https://pytorch.org/get-started/')
-            if code == 0:
-                print('Successfully installed PyTorch and torchvision! (If you need the GPU version, please install it manually, checkout the mindsdb docs and the pytorch docs if you need help)')
-        install.run(self)
-        
 # Windows specific requirements
+print(f"Platform: {sys.platform}")
+if sys.platform in ['win32','cygwin','windows']:
+    torch_version = "torch>=1.6.0,<2.0.0"
+    torchvision_version = "torchvision>=0.7.0,<1.0.0"
+
+    for requirement in install_reqs:
+        if "torch" in requirement:
+            torch_version = requirement
+        if "torchvision" in requirement:
+            torchvision_version = requirement
+    
+    install_reqs = remove_requirements(install_reqs,'torch')
+    install_reqs = remove_requirements(install_reqs,'torchvision')
+
+    print('Trying to install PyTorch and Torchvision!')
+    code = 1
+    try:
+        code = subprocess.call(['pip', 'install', torch_version, torchvision_version, '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+        if code != 0:
+            raise Exception('PyTorch and Torchvision installation failed !')
+    except:
+        try:
+            code = subprocess.call(['pip3', 'install', torch_version, torchvision_version, '-f', 'https://download.pytorch.org/whl/torch_stable.html'])
+            if code != 0:
+                raise Exception('PyTorch and Torchvision installation failed !')
+        except:
+            print('Failed to install PyTorch, please install PyTorch and Torchvision manually following the simple instructions at: https://pytorch.org/get-started/')
+    if code == 0:
+        print('Successfully installed PyTorch and torchvision! (If you need the GPU version, please install it manually, checkout the mindsdb docs and the pytorch docs if you need help)')
 
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
@@ -219,7 +211,4 @@ setup(
     # platform.
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
-    cmdclass={
-        'install': PreInstallCommand
-    }
 )
